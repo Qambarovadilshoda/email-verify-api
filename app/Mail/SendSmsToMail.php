f33eb17156406da233d8192c7c8283cc34c6395a
Dilshoda <qambarovadilshoda867@gmail.com>
2024-11-18 16:45:07 +0500

    I created mail for send sms to email user's

---
+++ app/Mail/SendSmsToMail.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Mail;
+
+use App\Models\User;
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class SendSmsToMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     */
+    public function __construct(protected User $user)
+    {
+        //
+    }
+
+    /**
+     * Get the message envelope.
+     */
+    public function envelope(): Envelope
+    {
+        return new Envelope(
+            subject: 'Send Sms To Mail',
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     */
+    public function content(): Content
+    {
+        $link = route('email.verify') . '?email-verify=' . $this->user->verification_token;
+        return new Content(
+            view: 'email.send',
+            with:[
+                'user' => $this->user,
+                'link' => $link
+            ]
+        );
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array<int, \Illuminate\Mail\Mailables\Attachment>
+     */
+    public function attachments(): array
+    {
+        return [];
+    }
+}

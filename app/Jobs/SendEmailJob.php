070268bd63144f9fc47e047522ed8d2642466303
Dilshoda <qambarovadilshoda867@gmail.com>
2024-11-19 12:35:41 +0500

    I create and wrote queue job for send sms to email

---
+++ app/Jobs/SendEmailJob.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\User;
+use App\Mail\SendSmsToMail;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Foundation\Queue\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class SendEmailJob implements ShouldQueue
+{
+    use Queueable;
+
+    /**
+     * Create a new job instance.
+     */
+    public function __construct(protected User $user)
+    {
+        //
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        Log::info($this->user);
+        Mail::to($this->user->email)->send(new SendSmsToMail($this->user));
+    }
+}
